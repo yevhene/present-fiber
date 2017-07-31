30c38bbaa415fdd6d88ccc6856c780e9a08c4421
Yevhen Shemet <yevhene@gmail.com>
2017-08-01 00:01:14 +0300

    Cleanup

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,7 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Map from './app/Map'
-import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<Map />, document.getElementById('root'))
-registerServiceWorker()
